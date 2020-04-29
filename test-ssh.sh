3965db44843922dfce6b52438072d20076d078a5
viveksaur <40718204+viveksaur@users.noreply.github.com>
2020-04-29 16:42:16 +0530

    Delete test-ssh.sh

--- test-ssh.sh
+++
@@ -1,9 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-rc7/aio/deploy/recommended.yaml
-kubectl get pods --namespace kubernetes-dashboard -o wide
-kubectl get svc --namespace kubernetes-dashboard -o wide
-
-
-
-# Dashboard creation
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-rc7/aio/deploy/recommended.yaml
-
